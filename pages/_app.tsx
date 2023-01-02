e0f22e112d2d8b456e74ab1a7aaf72b5aa0fab2c
gunwoongPark <bung1438@gmail.com>
2023-01-03 01:24:22 +0900

    feat: 페이지 구조 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,17 @@
 import type { AppProps } from "next/app";
-import { Fragment } from "react";
 import { Hydrate, QueryClientProvider } from "react-query";
 import queryClient from "../react-query";
 import { GlobalStyle } from "../styles/GlobalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Fragment>
+    <>
       <GlobalStyle />
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
         </Hydrate>
       </QueryClientProvider>
-    </Fragment>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
-export default function HomePage() {
+const HomePage = () => {
   return <></>;
-}
+};
+
+export default HomePage;
