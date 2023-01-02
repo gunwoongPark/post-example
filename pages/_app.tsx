f55708e90ae7b36239921a2cd4256204e80f590a
gunwoongPark <bung1438@gmail.com>
2023-01-03 00:14:23 +0900

    feat: styled-components 세팅 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import { GlobalStyle } from "../styles/GlobalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
